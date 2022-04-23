e0bf142715e1ebf715cc360f01af464827928bdb
teddy1565 <teddy1565@gmail.com>
2022-04-23 17:51:17 +0800

    simple code

---
+++ index.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int n;
+    while(scanf("%d", &n) != EOF) {
+        int *data = (int*) malloc(sizeof(int) * n);
+        for (int i=0;i<n;i++) {
+            scanf("%d", &data[i]);
+        }
+        free(data);
+        printf("\n");
+    }
+    return 0;
+}
