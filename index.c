7ed106bcb2c4d858f62a1dbf2bac3fd9d40d0a4d
teddy1565 <teddy1565@gmail.com>
2022-04-25 19:57:09 +0800

    update real index file

--- index.c
+++ index.c
@@ -1,16 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-    int n;
-    while(scanf("%d", &n) != EOF) {
-        int *data = (int*) malloc(sizeof(int) * n);
-        for (int i=0;i<n;i++) {
-            scanf("%d", &data[i]);
+int *sort(int *m, int length) {
+    for (int current=0;current<length;current++) {
+        int temp = 0;
+        for (int match=0;match<length;match++) {
+            if (match == current) {
+                continue;
+            }
+            if (m[current] < m[match]) {
+                temp = m[current];
+                m[current] = m[match];
+                m[match] = temp;
+                temp = 0;
+            }
         }
-        
-        free(data);
+    }
+    return m;
+}
+
+int main(void) {
+    int n=0;
+    while(scanf("%d", &n) != EOF) {
+        int *m = (int*) malloc(sizeof(int)*n);
+        for (int i=0;i<n;i++) {
+            scanf("%d", &m[i]);
+        }
+        int *answer = sort(m,n);
+        for (int i=0;i<n;i++) {
+            printf("%d ", answer[i]);
+        }
         printf("\n");
+        free(m);
     }
     return 0;
 }
